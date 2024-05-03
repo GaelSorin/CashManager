cf2bc81f34a61f7901e9450136f3cf4c9cff6cd4
unknown <gael.sorin@epitech.eu>
2024-05-03 12:07:16 +0200

    Begining of the interface of the shop

---
+++ terminal/src/products.ts
@@ -0,0 +1,60 @@
+import productGoldenSkull from './assets/golden_skull.png'
+import productAshenChest from './assets/ashen_chest.png'
+import productRageChest from './assets/rage_chest.png'
+import productChope from './assets/chope.png'
+import productViolon from './assets/violon.png'
+import productPirateWeapon from './assets/pirate_weapon.png'
+import productSword from './assets/sword.png'
+import productGold from './assets/gold.png'
+
+
+export const PORDUCTS = [
+    {
+        id: 1,
+        productName: "Golden Skull",
+        price : 50.0,
+        productImage: productGoldenSkull,
+    },
+    {
+        id: 2,
+        productName: "Ashen Chest",
+        price : 150.0,
+        productImage: productAshenChest,
+    },
+    {
+        id: 3,
+        productName: "Rage Chest",
+        price : 1000.0,
+        productImage: productRageChest,
+    },
+    {
+        id: 4,
+        productName: "Chope",
+        price : 10.0,
+        productImage: productChope,
+    },
+    {
+        id: 5,
+        productName: "Violon",
+        price : 75.0,
+        productImage: productViolon,
+    },
+    {
+        id: 6,
+        productName: "Pirate Weapons",
+        price : 450.0,
+        productImage: productPirateWeapon,
+    },
+    {
+        id: 7,
+        productName: "Sword",
+        price : 50.0,
+        productImage: productSword,
+    },
+    {
+        id: 8,
+        productName: "Gold",
+        price : 1.0,
+        productImage: productGold,
+    }
+]
